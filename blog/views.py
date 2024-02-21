b883e54043a99f003ec78657862000015db4a5fe
Roisin Ocon <roconnell11@icloud.com>
2024-02-21 09:50:12 +0000

    Add static directory, with sample css file, to style homepage

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,6 @@ from .models import Post
 
 # Create your views here - Class Based View, PostList
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
-
