cc263ae5378e1b807204ac9111d11cf90067130b
Roisin Ocon <roconnell11@icloud.com>
2024-02-21 09:41:23 +0000

    Add base template and blog template

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,6 @@ from .models import Post
 # Create your views here - Class Based View, PostList
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
 
