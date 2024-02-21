62b8c2409bef066216137b69e2d358fa36fb3999
Roisin Ocon <roconnell11@icloud.com>
2024-02-21 09:05:07 +0000

    Create generic Post View to view Blog Posts

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+# Create your views here - Class Based View, PostList
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
 
-
-def my_blog(request):
-    return HttpResponse("Hello, Jetset Journal Blog!")
