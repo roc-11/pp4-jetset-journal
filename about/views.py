6773b3664fc3defcb49bf7f3ab09c262f1a7488d
Roisin Ocon <roconnell11@icloud.com>
2024-03-04 20:14:12 +0000

    Add About App to project to display about page

---
+++ about/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import About
+
+
+def about_jetset_journal(request):
+    """
+    Renders the About page
+    """
+    about = About.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "about/about.html",
+        {"about": about},
+    )
--- jetset_journal/settings.py
+++ jetset_journal/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'django_summernote',
     'blog',
+    'about',
 ]
 
 # Auto redirect user to homepage after login/out
