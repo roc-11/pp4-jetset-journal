2d02eaea034ad61d0467186d8a5f37ddea417497
Roisin Ocon <roconnell11@icloud.com>
2024-03-21 13:12:55 +0000

    Link contact to crispy form

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from .forms import Contact, ContactForm
+from django.contrib import messages
+from .forms import ContactForm
 
 # Create your views here.
 def contact_jetset_journal(request):
+    if request.method == "POST":
+        contact_form = ContactForm(data=request.POST)
+        if contact_form.is_valid():
+            contact_form.save()
+            messages.add_message(request, messages.SUCCESS, "Contact request received! We'll try to respond within 2 working days.")
+
     contact_form = ContactForm()
 
     return render(
