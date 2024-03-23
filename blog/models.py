bc9baad5d5e86e6272b9ab0b5d5c0b4dd49ee0fb
Roisin Ocon <roconnell11@icloud.com>
2024-03-23 22:56:53 +0000

    Change comments to order by newest on DB

--- blog/models.py
+++ blog/models.py
@@ -58,7 +58,7 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ["created_on"]
+        ordering = ["-created_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
